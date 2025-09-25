6cceabdfcad13337a43aa7cfc484b88662b224c0
Nancy <wangui.kanyingi2015@gmail.com>
2025-09-25 18:38:58 +0300

    Adds changes

--- lib/a_name_error.py
+++ lib/a_name_error.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print(hello_world)
+print("hello world")
+
+2 * 2
+list = [0, 1, 2, 3, 4]
+dict = {'a':1, 'b':2, 'c':3}
+list[0]
+dict['c']
+
+'flatiron_school'
+
+wrong_type = 125 +123
+print(wrong_type)
